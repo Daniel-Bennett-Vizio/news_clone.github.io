c92601fd06fb6a77ca4cb0d4c13f0f260fb87d50
Daniel-Bennett-Vizio <85178301+Daniel-Bennett-Vizio@users.noreply.github.com>
2025-07-22 11:23:26 -0500

    update

--- pages/stories.js
+++ pages/stories.js
@@ -1,33 +1,33 @@
-import Story from '../components/Story.js'; 
+import Story from '../components/Story.js';
 import view from '../utils/view.js';
-import baseUrl from '../utils.baseUrl.js';
+import baseUrl from '../utils/baseUrl.js';
 
 export default async function Stories(path) {
-    const stories = await getStories(path)
-    const hasStories = stories.length > 0;
-
+  const stories = await getStories(path);
+  const hasStories = stories.length > 0;
+                    
   view.innerHTML = `<div>
-  ${hasStories ? stories.map((story, i) => Story({ ...story, index: i + 1 })).join('') : 'No stories'}
+    ${hasStories ? stories.map((story, i) => Story({ ...story, index: i + 1 })).join('') : 'No stories'}
   </div>`;  
 }
 
 async function getStories(path) {
   const isHomeRoute = path === '/';
-  const isNewRoute = path === '/new'; 
+  const isNewRoute = path === '/new';
   if (isHomeRoute) {
-    path = '/news';
+    path = '/news';  
   } else if (isNewRoute) {
-    path = '/newest'
+    path = '/newest';  
   } 
-  const response = await fetch(`${baseUrl}${path}`)
+  const response = await fetch(`${baseUrl}${path}`);
   const stories = await response.json();
   return stories;
 }
 
 // https://node-hnapi.herokuapp.com
 
-// / (Top) -> /new
-// /new (New) - /newest
+// / (Top) -> /news
+// /new (New) -> /newest
 // /ask (Ask) -> /ask
 // /show (Show) -> /show 
 // /jobs -> /jobs
